8798e6804538b0567e8f2c092651e683e8de9e0a
Peadar Jolliffe Byrne <peadarjb@gmail.com>
2024-04-30 21:37:06 +0100

    Trying to fix y-scroll and upland folk svg colour

--- src/pages/Year2022.js
+++ src/pages/Year2022.js
@@ -9,7 +9,7 @@ const Year2022 = () => {
         <div className="year-container">
             <div className="title-section">
             <picture>
-                <img src="public\images\2022\UplandFolk-black-large.svg" alt="Upland Folk title text"/>
+                <img src=".\images\2022\UplandFolk-black-large.svg" alt="Upland Folk title text"/>
             </picture>
             </div>
             <div className="text-section">
@@ -17,23 +17,14 @@ const Year2022 = () => {
                 The exhibition was made in response to the research Peadar carried out in 2021 during his work on the
                 UNESCO World Heritage Tentative List application for the cultural landscape of the Burren, Co. Clare, Ireland.
             </p>
-            </div>
-            <div className="text-section"> 
-                <p>
+            <p>
                 The Burren provides a plethora of inspirational starting points for any sort of artistic endeavour, ranging from its 
                 globally significant karst environment, to its extraordinary collection of well preserved archaeological sites, to the 
                 unique ecological mosaic of its flora and fauna. Yet perhaps, the most outstanding characteristic within this seemingly 
                 inhospitable landscape are the people who live there.
                 </p>
             </div>
-            <div className="text-section"> 
-                <p>
-                The Burren provides a plethora of inspirational starting points for any sort of artistic endeavour, ranging from its 
-                globally significant karst environment, to its extraordinary collection of well preserved archaeological sites, to the 
-                unique ecological mosaic of its flora and fauna. Yet perhaps, the most outstanding characteristic within this seemingly 
-                inhospitable landscape are the people who live there.
-                </p>
-            </div>
+
             <div>
             <Gallery year="2022" paintingsData={paintingsData["2022"]} />
             </div>
